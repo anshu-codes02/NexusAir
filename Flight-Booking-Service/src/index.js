54bc4ad5e8f9afcdb232f3d49c5706430bd1bb8f
Anshu Gupta <anshuacro@gmail.com>
2026-04-06 17:17:33 +0530

    feat: initialize Booking Service

---
+++ Flight-Booking-Service/src/index.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app=express();
+const {serverConfig}=require('./config');
+
+
+
+app.listen(serverConfig.PORT,()=>{
+    console.log("server running on port", serverConfig.PORT);
+});
