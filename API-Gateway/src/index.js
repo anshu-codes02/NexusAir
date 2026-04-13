02c879f9617d5a8f77adecd89544a260239bd3c4
Anshu Gupta <anshuacro@gmail.com>
2026-04-13 15:49:13 +0530

    feat: initialize API-Gateway

---
+++ API-Gateway/src/index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+const {serverConfig}=require('./config');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.listen(serverConfig.PORT, ()=>{
+    console.log(`API Gateway is running on port ${serverConfig.PORT}`);
+});
